b89eeccb4126ec7a49e2861c7365362c3e18fb1e
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2022-02-25 17:34:30 -0500

    added images

--- main.py
+++ main.py
@@ -42,17 +42,17 @@ PINK = (235, 65, 54)
 
 # load music and sounds
 pygame.mixer.music.load('audio/music2.mp3')
-pygame.mixer.music.set_volume(0.3)
-pygame.mixer.music.play(-1, 0.0, 5000)
+pygame.mixer.music.set_volume(0)
+#pygame.mixer.music.play(-1, 0.0, 0)
 
 jump_fx = pygame.mixer.Sound('audio/jump.wav')
-jump_fx.set_volume(0.5)
+jump_fx.set_volume(0)
 
 shot_fx = pygame.mixer.Sound('audio/shot.wav')
-shot_fx.set_volume(0.5)
+shot_fx.set_volume(0)
 
 grenade_fx = pygame.mixer.Sound('audio/grenade.wav')
-grenade_fx.set_volume(0.5)
+grenade_fx.set_volume(0)
 
 # images
 # store tiles in a list
